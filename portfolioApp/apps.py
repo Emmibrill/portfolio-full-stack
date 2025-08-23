e9088fa65262ecf5f641c9d182b07d693e9de418
Emmanue Okokon Udo <ubryliant@gmail.com>
2025-08-23 13:11:00 +0100

    edit and update procfile

--- portfolioApp/apps.py
+++ portfolioApp/apps.py
@@ -1,6 +1,19 @@
 from django.apps import AppConfig
+import os;
 
 
 class PortfolioappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'portfolioApp'
+
+    def ready(self):
+        from django.contrib.auth import get_user_model
+        User = get_user_model()
+
+        username = os.getenv("DJANGO_SUPERUSER_USERNAME")
+        email = os.getenv("DJANGO_SUPERUSER_EMAIL")
+        password = os.getenv("DJANGO_SUPERUSER_PASSWORD")
+
+        if username and password and not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username=username, email=email, password=password)
+            print(f"Superuser '{username}' created ✅")
